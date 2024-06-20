b848998dae49e85c70af4b2bfff4c5a9c81f0f3a
Ranul_112445 <ranul.gamage@axiatadigitallabs.com>
2024-06-20 13:38:03 +0530

    image not getting update issue fixed.set to Eastern US time to update

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import InfoNote from "./components/InfoNote";
 
 function App() {
   const [showDetails, setShowDetails] = useState(false);
-  const [data,setData]=useState(null);
+  const [data, setData] = useState(null);
   const [showInfoNote, setShowInfoNote] = useState(true);
-  const [loadingPage,setLoadingPage]=useState(false);
+  const [loadingPage, setLoadingPage] = useState(false);
 
-  function handelDetailPanel(){
+  function handelDetailPanel() {
     setShowDetails(!showDetails);
   }
 
@@ -19,44 +19,51 @@ function App() {
     setShowInfoNote(false);
   }
 
-  useEffect(()=>{
-    async function fetchAPIData(){
+  useEffect(() => {
+    async function fetchAPIData() {
       const NASA_API_KEY = import.meta.env.VITE_NASA_API_KEY;
-      const url = 'https://api.nasa.gov/planetary/apod'+`?api_key=${NASA_API_KEY}`
-      const today=(new Date()).toDateString;
-      const localKey=`NASA-APOD-${today}`;
-      if(localStorage.getItem(localKey)){
-        const apiData=JSON.parse(localStorage.getItem(localKey));
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_API_KEY}`
+      let currentDate = new Date();
+      let options = {
+        timeZone: 'America/New_York',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      };
+      let today = currentDate.toLocaleDateString('en-US', options);
+      const localKey = `NASA-APOD-${today}`;
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
         setData(apiData);
         console.log("Fetch Data From Browser cache");
         return;
       }
-      try{
-        if(data==null){
+      try {
+        if (data == null) {
           localStorage.clear();
-          const response=await fetch(url);
-          const dataAPI=await response.json();
-          localStorage.setItem(localKey,JSON.stringify(dataAPI));
+          const response = await fetch(url);
+          const dataAPI = await response.json();
+          localStorage.setItem(localKey, JSON.stringify(dataAPI));
           setData(dataAPI);
-          console.log("Fetch Data From NASA API DATA\n",dataAPI);
+          console.log("Fetch Data From NASA API DATA\n", dataAPI);
         }
-      }catch(e){
+      } catch (e) {
         console.log(e.message);
       }
     }
     fetchAPIData();
-  },[]);
+  }, []);
   return (
     <>
-        {showInfoNote && <InfoNote handleClose={handleCloseInfoNote} />}
-      {data ? (<Main data={data}  />) :( 
+      {showInfoNote && <InfoNote handleClose={handleCloseInfoNote} />}
+      {data ? (<Main data={data} />) : (
         <div className="loadingPage">
-          <ReactLoading  type="spinningBubbles" color="#007bff" height={100} width={100} />
+          <ReactLoading type="spinningBubbles" color="#007bff" height={100} width={100} />
           <h1>Loading...</h1>
         </div>
       )}
       {showDetails && (<SideBar data={data} handelDetailPanel={handelDetailPanel} />)}
-      {data && (<Footer data={data} handelDetailPanel={handelDetailPanel} />) }
+      {data && (<Footer data={data} handelDetailPanel={handelDetailPanel} />)}
     </>
   )
 }
